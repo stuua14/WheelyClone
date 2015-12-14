68b3a2e5fd28fec8fb8eb0c7dc0e963fa1050418
isles3536 <isles3536@HRH-IC0027327.wrdsb.ca>
2015-12-14 10:37:21 -0500

    PepeForPresident

---
+++ WheelyClone/src/wheelyclone/WheelyClone.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wheelyclone;
+
+/**
+ *
+ * @author isles3536
+ */
+public class WheelyClone {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int Ilovedankmemes;
+    }
+}
